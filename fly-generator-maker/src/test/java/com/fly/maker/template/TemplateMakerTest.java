e38dc089ac23cba3da238390b3128e87cdace647
fly <1785613048@qq.com>
2024-01-07 16:38:22 +0800

    添加 用户自定义是否选择需要cors的json配置，编写对应的测试类

--- fly-generator-maker/src/test/java/com/fly/maker/template/TemplateMakerTest.java
+++ fly-generator-maker/src/test/java/com/fly/maker/template/TemplateMakerTest.java
@@ -113,6 +113,11 @@ public class TemplateMakerTest {
         configStr = ResourceUtil.readUtf8Str(rootPath + "templateMaker2.json");
         templateMakerConfig = JSONUtil.toBean(configStr, TemplateMakerConfig.class);
         TemplateMaker.makeTemplate(templateMakerConfig);
+
+
+        configStr = ResourceUtil.readUtf8Str(rootPath + "templateMaker3.json");
+        templateMakerConfig = JSONUtil.toBean(configStr, TemplateMakerConfig.class);
+        TemplateMaker.makeTemplate(templateMakerConfig);
         System.out.println(id);
     }
 
