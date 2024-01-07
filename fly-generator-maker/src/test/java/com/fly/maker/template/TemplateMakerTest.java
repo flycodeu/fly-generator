c5b9dc3b2d16a12540ef7574ed7c0f7976bc6187
fly <1785613048@qq.com>
2024-01-07 10:37:14 +0800

    新建测试，测试bug

---
+++ fly-generator-maker/src/test/java/com/fly/maker/template/TemplateMakerTest.java
@@ -0,0 +1,87 @@
+package com.fly.maker.template;
+
+import com.fly.maker.meta.Meta;
+import com.fly.maker.template.enums.FileFilterRangeEnum;
+import com.fly.maker.template.enums.FileFilterRuleEnum;
+import com.fly.maker.template.model.FileFilterConfig;
+import com.fly.maker.template.model.TemplateMakerFileConfig;
+import com.fly.maker.template.model.TemplateMakerModelConfig;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * 测试bug
+ */
+public class TemplateMakerTest {
+    @Test
+    public void testMakeTemplateBug1() {
+        Meta meta = new Meta();
+        meta.setName("acm-template-generator");
+        meta.setDescription("ACM 示例模板生成器");
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "fly-generator-demo-projects/springboot-init";
+
+        String inputFilePath1 = "/src/main/resources/application.yml";
+
+        List<FileFilterConfig> fileFilterConfigList = new ArrayList<>();
+        // 文件过滤配置
+        TemplateMakerFileConfig.FileInfoConfig fileInfoConfig1 = new TemplateMakerFileConfig.FileInfoConfig();
+        fileInfoConfig1.setFileFilterConfigs(fileFilterConfigList);
+        fileInfoConfig1.setPath(inputFilePath1);
+        TemplateMakerFileConfig templateMakerFileConfig = new TemplateMakerFileConfig();
+        templateMakerFileConfig.setFiles(Collections.singletonList(fileInfoConfig1));
+
+        // 模型配置
+        TemplateMakerModelConfig templateMakerModelConfig = new TemplateMakerModelConfig();
+        TemplateMakerModelConfig.ModelInfoConfig modelInfoConfig1 = new TemplateMakerModelConfig.ModelInfoConfig();
+        modelInfoConfig1.setFieldName("url");
+        modelInfoConfig1.setType("String");
+        modelInfoConfig1.setDefaultValue("jdbc:mysql://localhost:3306/my_db");
+        modelInfoConfig1.setReplaceText("jdbc:mysql://localhost:3306/my_db");
+
+        List<TemplateMakerModelConfig.ModelInfoConfig> modelInfoConfigList = Arrays.asList(modelInfoConfig1);
+        templateMakerModelConfig.setModels(modelInfoConfigList);
+
+        long l = TemplateMaker.makeTemplate(meta, originProjectPath, templateMakerFileConfig, templateMakerModelConfig, 1743817327068332032L);
+        System.out.println(l);
+    }
+
+    @Test
+    public void testMakeTemplateBug2() {
+        Meta meta = new Meta();
+        meta.setName("acm-template-generator");
+        meta.setDescription("ACM 示例模板生成器");
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "fly-generator-demo-projects/springboot-init";
+
+        String inputFilePath1 = "/src/main/java/com/yupi/springbootinit/common";
+
+        List<FileFilterConfig> fileFilterConfigList = new ArrayList<>();
+        // 文件过滤配置
+        TemplateMakerFileConfig.FileInfoConfig fileInfoConfig1 = new TemplateMakerFileConfig.FileInfoConfig();
+        fileInfoConfig1.setFileFilterConfigs(fileFilterConfigList);
+        fileInfoConfig1.setPath(inputFilePath1);
+        TemplateMakerFileConfig templateMakerFileConfig = new TemplateMakerFileConfig();
+        templateMakerFileConfig.setFiles(Collections.singletonList(fileInfoConfig1));
+
+        // 模型配置
+        TemplateMakerModelConfig templateMakerModelConfig = new TemplateMakerModelConfig();
+        TemplateMakerModelConfig.ModelInfoConfig modelInfoConfig1 = new TemplateMakerModelConfig.ModelInfoConfig();
+        modelInfoConfig1.setFieldName("className");
+        modelInfoConfig1.setType("String");
+        modelInfoConfig1.setReplaceText("BaseResponse");
+
+        List<TemplateMakerModelConfig.ModelInfoConfig> modelInfoConfigList = Arrays.asList(modelInfoConfig1);
+        templateMakerModelConfig.setModels(modelInfoConfigList);
+
+        long l = TemplateMaker.makeTemplate(meta, originProjectPath, templateMakerFileConfig, templateMakerModelConfig, 1743817327068332032L);
+        System.out.println(l);
+    }
+}
