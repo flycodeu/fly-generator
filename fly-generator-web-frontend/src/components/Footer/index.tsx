a52f9ed3bd5fed3f2dbb5c5bcb74f30fb5d3d01c
fly <1785613048@qq.com>
2024-01-11 14:36:24 +0800

    修复

--- fly-generator-web-frontend/src/components/Footer/index.tsx
+++ fly-generator-web-frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import '@umijs/max';
 import React from 'react';
 
 const Footer: React.FC = () => {
-  const defaultMessage = '程序员鱼皮';
+  const defaultMessage = '程序员飞云';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
@@ -15,24 +15,18 @@ const Footer: React.FC = () => {
       links={[
         {
           key: 'codeNav',
-          title: '编程导航',
-          href: 'https://yupi.icu',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: '编程宝典',
-          href: 'https://codefather.cn',
+          title: '飞云编程',
+          href: 'https://flycode.icu',
           blankTarget: true,
         },
         {
           key: 'github',
           title: (
             <>
-              <GithubOutlined /> 鱼皮源码
+              <GithubOutlined /> 飞云源码
             </>
           ),
-          href: 'https://github.com/liyupi',
+          href: 'https://github.com/flybase1',
           blankTarget: true,
         },
       ]}
